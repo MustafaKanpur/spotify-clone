0bb34921a2bcc2aae44aa0ca9826f87a7531d927
Mustafa Kanpurwala <mkanpurw@uwo.ca>
2025-01-02 16:17:39 -0500

    Changed bg colour

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const {audioRef, track} = useContext(PlayerContext);
 
   return(
-    <div className='h-screen bg-black'>
+    <div className='h-screen bg-white'>
 
       <TopBar/>
       <div className='h-[90%] flex'>
